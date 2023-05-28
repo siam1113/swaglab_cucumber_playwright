bce80171882e187b9b5bf124c998137c23547d0a
SherlockHolmes2022 <sherlockholmessh8@gmail.com>
2023-05-28 15:43:32 +0600

    Login test

---
+++ tests/acceptance/features/login/login.feature
@@ -0,0 +1,10 @@
+Feature: Login
+  Scenario: Login with username and password
+    Given I am on the login page for valid login
+    When I enter username and password
+    Then I should be on the home page
+
+  Scenario: Login with invalid username and password
+    Given I am on the login page for invalid login
+    When I enter invalid username and password
+    Then I should be in home page
---
+++ tests/acceptance/stepDefinitions/login/login.js
@@ -0,0 +1,30 @@
+const { Given, When, Then } = require("@cucumber/cucumber");
+const { expect } = require("@playwright/test");
+
+Given("I am on the login page for valid login", async function () {
+  await page.goto("https://www.saucedemo.com/");
+});
+
+When("I enter username and password", async function () {
+  await page.fill("#user-name", "standard_user");
+  await page.fill("#password", "secret_sauce");
+  await page.click("#login-button");
+});
+
+Then("I should be on the home page", async function () {
+  await page.waitForSelector("#inventory_container");
+});
+
+Given("I am on the login page for invalid login", async function () {
+  await page.goto("https://www.saucedemo.com/");
+});
+
+When("I enter invalid username and password", async function () {
+  await page.fill("#user-name", "standard_use");
+  await page.fill("#password", "secret_sauc");
+  await page.click("#login-button");
+});
+
+Then("I should be in home page", async function () {
+  await expect(page).toHaveURL("https://www.saucedemo.com/");
+});
